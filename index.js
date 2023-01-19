045d302621aa1b3c2ff7f5cfdfbb2b75c85015e5
ash50 <aldi.iman10@gmail.com>
2023-01-19 22:46:07 +0700

    Upload ke2 buat tugas

---
+++ index.js
@@ -0,0 +1,8 @@
+let Satu = document.querySelector('#Satu');
+let Dua = document.querySelector('#Dua');
+let Tiga = document.querySelector('#Tiga');
+let bg = document.querySelector('#bg');
+
+Satu.addEventListener('click', () => bg.style.backgroundColor='#337ab7')
+Dua.addEventListener('click', () => alert('Hay, mencoba JavaScript'))
+Tiga.addEventListener('click', () => Tiga.style.backgroundColor='#7FFFD4')
